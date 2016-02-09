583f684c1bf89d2b138de39b18e868340be1a576
Mantas Grauzinis <anonimo@inbox.lt>
2016-02-09 12:26:45 +0200

    simplified validation

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -8,26 +8,22 @@ class Validator {
         $errors = [];
         
         foreach($validation_data as $name => $value) {
-            if(isset($_REQUEST[$name])) {
-                $exploded = explode(":", $value);
-            
-                switch($exploded[0]) {
-                    case 'min':
-                        $min = $exploded[1];
-                        if(Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false) {
-                            $errors[] = $name . " must be at leat" . $min . " chearacters long";
-                        }
-                        break;
-                    case 'email':
-                        if(Valid::email()->Validate($_REQUEST[$name]) == false) {
-                            $errors[] = "Email address is invalid!";
-                        }
-                        break; 
-                    default: 
-                        // do nothing
-                }
-            } else {
-                $errors[] = "No value found!";
+            $exploded = explode(":", $value);
+
+            switch($exploded[0]) {
+                case 'min':
+                    $min = $exploded[1];
+                    if(Valid::stringType()->length($min)->Validate($_REQUEST[$name]) == false) {
+                        $errors[] = $name . " must be at leat" . $min . " chearacters long";
+                    }
+                    break;
+                case 'email':
+                    if(Valid::email()->Validate($_REQUEST[$name]) == false) {
+                        $errors[] = "Email address is invalid!";
+                    }
+                    break; 
+                default: 
+                    $errors[] = "No value found!";
             }
         }
         return $errors;
